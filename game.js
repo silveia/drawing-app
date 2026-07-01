0c98896171a12283813e7ae9ee0cd700cfb84365
silveia <sil.gacha.obsession@gmail.com>
2026-07-01 15:37:57 -0700

    Add visual hitbox element variable
    Added a visual hitbox element variable to global variables.

--- game.js
+++ game.js
@@ -3,6 +3,17 @@
 // ==========================================================================
 const bgWorld = document.getElementById('gameWorldBackground');
 const playerEl = document.getElementById('gamePlayer');
+
+
+
+
+let visualHitboxEl = null; // Add this with your other global variables
+
+
+
+
+
+
 let isPlaying = false;
 let gameLoopId = null;
 
